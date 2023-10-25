7710d5488f84114f38a3bdad25d4c760e081be2e
ivanoVucak <145533812+ivanoVucak@users.noreply.github.com>
2023-10-25 09:15:23 +0200

    Add files via upload

---
+++ klasaStringP4/Program.cs
@@ -0,0 +1,26 @@
+﻿
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace klasaStringP4
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string tekst;
+            Console.WriteLine("Unesi niz znakova: ");
+            tekst = Convert.ToString(Console.ReadLine());
+
+            string[] rijeci = tekst.Split(' ');
+           
+            Console.Write(rijeci.First() + " ");
+            Console.WriteLine(rijeci.Last());
+
+            Console.ReadKey();
+        }
+    }
+}
